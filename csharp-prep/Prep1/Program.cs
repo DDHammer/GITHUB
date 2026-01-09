e319032d68e0d0edf2303990c0851fb6b210dfa6
DDhammer <ham21034@byui.edu>
2026-01-08 17:43:29 -0700

    Update: Grade tracker

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -9,6 +9,6 @@ class Program
         string First=Console.ReadLine();
         Console.WriteLine("What is your last name?");
         string Last=Console.ReadLine();
-        Console.WriteLine($"Your name is {Last},{First} {Last}.");
+        Console.WriteLine($"Your name is {Last}, git{First} {Last}.");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string percent = Console.ReadLine();
+        int percent2 = int.Parse(percent);
+
+        string letter = "";
+
+        if (percent2 >= 90)
+        {
+            letter = "A";
+        }
+        else if (percent2 >= 80)
+        {
+            letter = "B";
+        }
+        else if (percent2 >= 70)
+        {
+            letter = "C";
+        }
+        else if (percent2 >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+        if (percent2 >= 70)
+        {
+        Console.WriteLine("Congratulations! You passed the course.");
+        }
+        else
+        {
+        Console.WriteLine("You got it next time.");
+        }
     }
-}
+};
