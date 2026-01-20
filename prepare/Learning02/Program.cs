5037afab0ac844d5d3d67d61a0ae460a155a0f45
DDhammer <ham21034@byui.edu>
2026-01-20 16:13:27 -0700

    Update: Resume help

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,59 @@
 using System;
 
-class Program
+public class Job
+{
+    public string _jobTitle;
+    public string _company;
+    public int _startYear;
+    public int _endYear;
+    public void Display()
+    {
+        Console.WriteLine($"{_jobTitle} ({_company}) Years/Year:{_startYear} to {_endYear}");
+    }
+}
+public class Resume
+{
+    public string _name;
+
+    public List<Job> _jobs = new List<Job>();
+    public void Display()
+    {
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine("Jobs:");
+
+        foreach (Job job in _jobs)
+        {
+            job.Display();
+        }
+    }
+}class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "College student";
+        job1._company = "BYUI";
+        job1._startYear = 2024;
+        job1._endYear = 2028;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Screen Printer";
+        job2._company = "Textile";
+        job2._startYear = 2025;
+        job2._endYear = 2026;
+
+        Job job3 = new Job();
+        job3._jobTitle = "Customer Service";
+        job3._company = "Melaeuca";
+        job3._startYear = 2024;
+        job3._endYear = 2025;
+
+        Resume myResume = new Resume();
+        myResume._name = "Deagan Hammer";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        myResume._jobs.Add(job3);
+        myResume.Display();
     }
 }
+
