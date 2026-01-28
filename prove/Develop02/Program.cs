98feb46c487901eb0c75353e9d7f24061364cf49
DDhammer <ham21034@byui.edu>
2026-01-28 13:06:31 -0700

    Update: Finished Journal Entry (With Mood Tracker)

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,154 @@
 using System;
 
+// THE ENTRY CLASS
+public class Entry
+{
+    public string _date;
+    public string _promptText;
+    public string _entryText;
+    public string _mood;
+
+    public void Display()
+    {
+        Console.WriteLine($"Date: {_date} - Prompt: {_promptText}");
+        Console.WriteLine($"Mood: {_mood}");
+        Console.WriteLine($"{_entryText}");
+        Console.WriteLine("----------------------------------------");
+    }
+}
+// THE PROMPT GENERATOR CLASS
+public class PromptGenerator
+{
+    public List<string> _prompts = new List<string>
+    {
+        "Who was the most interesting person I interacted with today?",
+        "What was the best part of my day?",
+        "How did I see the hand of the Lord in my life today?",
+        "What was something fun you did today?",
+        "If I had one thing I could do over today, what would it be?",
+        "What do you want to do tomorrow?",
+        "What is the most fun thing you did today?"
+    };
+
+    public string GetRandomPrompt()
+    {
+        Random random = new Random();
+        int index = random.Next(_prompts.Count);
+        return _prompts[index];
+    }
+}
+// THE JOURNAL CLASS
+public class Journal
+{
+    public List<Entry> _entries = new List<Entry>();
+    public void AddEntry(Entry newEntry)
+    {
+        _entries.Add(newEntry);
+    }
+    public void DisplayAll()
+    {
+        foreach (Entry entry in _entries)
+        {
+            entry.Display();
+        }
+    }
+    public void SaveToFile(string file)
+    {
+        using (StreamWriter outputFile = new StreamWriter(file))
+        {
+            foreach (Entry entry in _entries)
+            {
+                outputFile.WriteLine($"{entry._date}|{entry._promptText}|{entry._entryText}|{entry._mood}");
+            }
+        }
+        Console.WriteLine("Journal saved successfully!");
+    }
+
+    public void LoadFromFile(string file)
+    {
+        if (!File.Exists(file))
+        {
+            Console.WriteLine("Unable to find file. You may have the wrong name!");
+            return;
+        }
+        _entries.Clear(); 
+        string[] lines = File.ReadAllLines(file);
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split("|");
+
+            if (parts.Length == 4)
+            {
+                Entry newEntry = new Entry();
+                newEntry._date = parts[0];
+                newEntry._promptText = parts[1];
+                newEntry._entryText = parts[2];
+                newEntry._mood = parts[3];
+                _entries.Add(newEntry);
+            }
+        }
+        Console.WriteLine("Loaded Journal!");
+    }
+}
+// THE MAIN PROGRAM
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Journal theJournal = new Journal();
+        PromptGenerator promptGen = new PromptGenerator();
+        string choice = "";
+
+        Console.WriteLine("Welcome to the Daily Reflection Journal!");
+
+        while (choice != "5")
+        {
+            Console.WriteLine("\nPlease select one of the following choices:");
+            Console.WriteLine("1. Write New Entry");
+            Console.WriteLine("2. Display Journal");
+            Console.WriteLine("3. Load Journal");
+            Console.WriteLine("4. Save Journal");
+            Console.WriteLine("5. Quit");
+            Console.Write("What would you like to do? ");
+            choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+                string prompt = promptGen.GetRandomPrompt();
+                Console.WriteLine($"\nPROMPT: {prompt}");
+                Console.Write("> ");
+                string response = Console.ReadLine();
+
+                Console.Write("How are you feeling today (Happy, Tired, Exicted)? ");
+                string mood = Console.ReadLine();
+
+                Entry newEntry = new Entry();
+                newEntry._date = DateTime.Now.ToShortDateString();
+                newEntry._promptText = prompt;
+                newEntry._entryText = response;
+                newEntry._mood = mood;
+
+                theJournal.AddEntry(newEntry);
+            }
+            else if (choice == "2")
+            {
+                theJournal.DisplayAll();
+            }
+            else if (choice == "3")
+            {
+                Console.Write("What is the filename? ");
+                string fileName = Console.ReadLine();
+                theJournal.LoadFromFile(fileName);
+            }
+            else if (choice == "4")
+            {
+                Console.Write("What is the filename? ");
+                string fileName = Console.ReadLine();
+                theJournal.SaveToFile(fileName);
+            }
+        }
+
+        Console.WriteLine("Come back soon to write more.");
     }
 }
+// Added a function to track the mood for the creativity part of the program
