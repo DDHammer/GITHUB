817ff414798d115a88e83b8f38577d7029a5a42e
DDhammer <ham21034@byui.edu>
2026-03-04 19:46:51 -0700

    done

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,67 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+namespace Learning05
 {
-    static void Main(string[] args)
+    public abstract class Shape
     {
-        Console.WriteLine("Hello Learning05 World!");
+        private string _color;
+        public Shape(string color)
+        {
+            _color = color;
+        }
+
+        public string GetColor() => _color;
+        public void SetColor(string color) => _color = color;
+        public abstract double GetArea();
+    }
+    public class Square : Shape
+    {
+        private double _side;
+        public Square(string color, double side) : base(color)
+        {
+            _side = side;
+        }
+        public override double GetArea() => _side * _side;
+    }
+    public class Rectangle : Shape
+    {
+        private double _length;
+        private double _width;
+
+        public Rectangle(string color, double length, double width) : base(color)
+        {
+            _length = length;
+            _width = width;
+        }
+        public override double GetArea() => _length * _width;
+    }
+    public class Circle : Shape
+    {
+        private double _radius;
+        public Circle(string color, double radius) : base(color)
+        {
+            _radius = radius;
+        }
+        public override double GetArea() => Math.PI * Math.Pow(_radius, 2);
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Shape> shapes = new List<Shape>();
+            shapes.Add(new Square("Red", 5));
+            shapes.Add(new Rectangle("Blue", 10, 2));
+            shapes.Add(new Circle("Green", 3));
+            Console.WriteLine("Shape Area Report:");
+            Console.WriteLine("------------------");
+            foreach (Shape s in shapes)
+            {
+                string color = s.GetColor();
+                double area = s.GetArea();
+
+                Console.WriteLine($"The {color} shape has an area of {area:F2}.");
+            }
+        }
     }
 }
