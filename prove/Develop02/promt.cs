4282b407e5f9e601719305f810da6a208e5ab3a2
DDhammer <ham21034@byui.edu>
2026-03-25 13:22:26 -0600

    Finished

---
+++ prove/Develop02/promt.cs
@@ -0,0 +1,20 @@
+// THE PROMPT GENERATOR CLASS
+public class PromptGenerator
+{
+    public List<string> _prompts = new List<string>
+    {
+        "Who was the most interesting person I interacted with today?",
+        "What was the best part of my day?",
+        "How did I see the hand of the Lord in my life today?",
+        "What was something fun you did today?",
+        "If I had one thing I could do over today, what would it be?",
+        "What do you want to do tomorrow?",
+        "What is the most fun thing you did today?"
+    };
+    public string GetRandomPrompt()
+    {
+        Random random = new Random();
+        int index = random.Next(_prompts.Count);
+        return _prompts[index];
+    }
+}
---
+++ prove/Develop03/scripture.cs
@@ -0,0 +1,32 @@
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _words = text.Split(' ').Select(w => new Word(w)).ToList();
+    }
+    public void HideRandomWords(int numberToHide)
+    {
+        Random random = new Random();
+        List<Word> visibleWords = _words.Where(w => !w.IsHidden()).ToList();
+        int actualToHide = Math.Min(numberToHide, visibleWords.Count);
+        for (int i = 0; i < actualToHide; i++)
+        {
+            int index = random.Next(visibleWords.Count);
+            visibleWords[index].Hide();
+            visibleWords.RemoveAt(index);
+        }
+    }
+    public string GetDisplayText()
+    {
+        string scriptureText = string.Join(" ", _words.Select(w => w.GetDisplayText()));
+        return $"{_reference.GetDisplayText()} {scriptureText}";
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        return _words.All(w => w.IsHidden());
+    }
+}
